0abd35ad6ebac638b78b44a40bb727271f8ecd87
Otávio Londero <otavio.b.londero@gmail.com>
2025-02-25 20:12:05 -0800

    Add lab06 and minor change in lab 5

--- Lab 5 - Matplotlib/E06Londero.py
+++ Lab 5 - Matplotlib/E06Londero.py
@@ -8,21 +8,26 @@ y = [1, 4, 9, 16, 25]
 plt.subplot(2,3,1)
 plt.plot(x, y)
 plt.title('Line Plot')
+
 #Bar Plot
 categories = ['A', 'B', 'C', 'D', 'E']
 values = [5, 7, 3, 8, 6]
-plt.subplot(2,3,2)
+plt.subplot(2,2,2)
 plt.bar(categories, values)
 plt.title('Bar Plot')
+
 #Histogram
 data = np.random.randn(1000) #(1000 random values)
-plt.subplot(2,3,3)
+plt.subplot(2,2,3)
 plt.hist(data)
 plt.title('Histogram')
+
 #Scatter Plot
 x_scatter = np.random.rand(50)
 y_scatter = np.random.rand(50)
-plt.subplot(2,3,4)
+plt.subplot(2,2,4)
 plt.scatter(x_scatter, y_scatter)
 plt.title('Scatter Plot')
+
+plt.subplots_adjust(hspace=0.3)
 plt.show()
